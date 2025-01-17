53e80b75817d11e0d6bbba5f946fcee1a2fc1725
Hi there! , I'm Gaurav <gsmagar16122003@gmail.com>
2025-01-17 11:39:43 +0530

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Gaurav Magar</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} GM</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/gaurav1603"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://leetcode.com/u/gauravsm_16/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <SiLeetcode />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href=" https://www.linkedin.com/in/gaurav-magar-4a3159232/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/gsmagar1622003/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+import studynotion from "../../Assets/Projects/studynotion.png";
+import eye from "../../Assets/Projects/eye.png";
+import facial1 from "../../Assets/Projects/facial1.jpeg";
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={studynotion}
+              isBlog={false}
+              title="Study-Notion"
+              description="StudyNotion is a comprehensive e-learning platform designed to make education accessible and engaging. It offers a diverse range of courses, interactive learning tools, and expert-led tutorials tailored to meet the needs of students and professionals. With features like progress tracking and community support, StudyNotion fosters a collaborative and personalized learning experience."
+              // ghLink="https://github.com/gaurav1603/Chatify"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={eye}
+              isBlog={false}
+              title="Third-Eye"
+              description="Third Eye is an innovative forensic analysis tool designed for enhanced facial recognition and sketch-based identification. It leverages advanced AI algorithms to process and analyze forensic sketches, aiding law enforcement in identifying individuals quickly and accurately. Third Eye streamlines investigations by providing intuitive insights and improving the efficiency of criminal profiling."
+              // ghLink="https://github.com/gaurav1603/Bits-0f-C0de"
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Editor.io"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              // ghLink="https://github.com/gaurav1603/Editor.io"
+              // demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={facial1}
+              isBlog={false}
+              title="Facial Recognition Attendance System"
+              description="The Facial Recognition Attendance System is a cutting-edge solution that automates attendance tracking using facial recognition technology. It captures and verifies facial data in real-time, ensuring accurate and efficient attendance management. This system eliminates the need for manual processes, enhances security, and provides seamless integration with organizational databases for easy reporting and analytics."
+              // ghLink="https://github.com/gaurav1603/Plant_AI"
+              // demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
